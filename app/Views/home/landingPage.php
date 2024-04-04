e8a94fac0161565c0e24d398a56c9a790337688c
isdahami <isdahami@iu.edu>
2024-04-03 22:20:17 -0400

    updated landing page

--- app/Views/home/landingPage.php
+++ app/Views/home/landingPage.php
@@ -8,16 +8,20 @@
     <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
     <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
     <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
     </ol>
     <div class="carousel-inner">
         <div class="carousel-item active">
             <img class="d-block w-100" src="<?= base_url('assets/images/hero-images/KISS.jpg')?>" alt="First slide">
         </div>
         <div class="carousel-item">
-            <img class="d-block w-100" src="<?= base_url('assets/images/hero-images/offspring.jpg')?>" alt="First slide">
+            <img class="d-block w-100" src="<?= base_url('assets/images/hero-images/offspring.jpg')?>" alt="Second slide">
         </div>
         <div class="carousel-item">
-            <img class="d-block w-100" src="<?= base_url('assets/images/hero-images/RS.jpg')?>" alt="First slide">
+            <img class="d-block w-100" src="<?= base_url('assets/images/hero-images/RS.jpg')?>" alt="Third slide">
+        </div>
+        <div class="carousel-item">
+            <img class="d-block w-100" src="<?= base_url('assets/images/hero-images/trending.jpg')?>" alt="Fourth slide">
         </div>
     </div>
         <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
